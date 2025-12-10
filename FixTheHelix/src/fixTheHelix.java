83f57f3c391e7c6213f694fe9291f82d33107555
feliciannadesantos <feliciannadesantos@feliciannas-air.lan>
2025-12-09 20:05:25 -0700

    began project

--- FixTheHelix/src/fixTheHelix.java
+++ FixTheHelix/src/fixTheHelix.java
@@ -1,14 +1,21 @@
-
+import java.util.Scanner;
 public class fixTheHelix
 	{
 
 		public static void main(String[] args)
 			{
-				
+			 Scanner userStringInput = new Scanner(System.in);
 				System.out.println("Enter DNA Sequence:");
-				System.out.println("Made another change.");
-				System.out.println("made change from home");
+				String dna= userStringInput.nextLine();
+				for(int i = 0; i < dna.length(); i++) {
+				    
+				if(dna.substring(i,i+1).equals("A")) {
+					System.out.print("T");
+				} else if(dna.equals("C")) {
+					System.out.print("G");
+				} 
 
 			}
 
 	}
+}
